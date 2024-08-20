08c43253c0eaee877dedcf035359cf16d4c35a9a
Ryan Wiratara Prasetyo <ryanwpansal@gmail.com>
2024-08-21 02:58:39 +0700

    update audio

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let vid = document.getElementById("audio");
-vid.volume = 0.5;
+let btnplay = document.getElementById("btnplay");
+let popup = document.getElementById("popup");
+let audio = document.getElementById("audio");
+let anya1 = document.getElementById("anya1");
+let anya2 = document.getElementById("anya2");
+
+btnplay.addEventListener("click", function () {
+  audio.play();
+  audio.volume = 0.4;
+  popup.style.display = "none";
+  anya1.style.display = "block";
+  anya2.style.display = "block";
+});
 
 AOS.init();
